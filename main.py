619ffa7bbf8a0c810b4f3761f496d68977700cd1
Nikolay.Nikolov2 <nikolay.nikolov2@adastragrp.com>
2022-02-03 23:37:55 +0200

    f

--- main.py
+++ main.py
@@ -25,15 +25,22 @@ class RecipeNutritionInformation(TypedDict):
 
 
 def get_nutrition_from_spoonacular(recipe_name: str) -> RecipeNutritionInformation:
+    # request response for 'recipe_name' from the API
     response = api.guess_nutrition_by_dish_name(recipe_name)
+    # translating from json
     data = response.json()
     return data
 
 
-spaghetti = get_nutrition_from_spoonacular("Spaghetti Aglio et Olio")
-print(spaghetti)
+# getting the name of the recipe / ex: Spaghetti Aglio et Olio
+recipe_check = get_nutrition_from_spoonacular(
+    input("What's the name of the recipe you want to get the nutritional information of?" + '\n'))
+print(recipe_check)
 
-# # Parse an ingredient
+# spaghetti = get_nutrition_from_spoonacular("Spaghetti Aglio et Olio")
+# print(spaghetti)
+
+# Parse an ingredient
 # response = api.visualize_recipe_nutrition("pasta", 2)
 # data = response.json()
 # print(data)
@@ -41,16 +48,3 @@ print(spaghetti)
 # response = api.get_a_random_food_joke()
 # data = response.json()
 # print(data['text'])
-#
-#
-# rec1 = get_nutrition_from_spoonacular("mangusta")
-# print(rec1)
-#
-# snack = RecipeNutritionInformation(recipes_used=2, calories=300, fat=50, protein=204, carbs=100)
-# snack = get_nutrition_from_spoonacular("yasha")
-# print(snack)
-#
-# snack1: RecipeNutritionInformation = dict(recipes_used=2, calories=300, fat=50)
-# print(snack1)
-#
-# RecipeNutritionInformation.recipe
